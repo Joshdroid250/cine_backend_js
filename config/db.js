22ed61a2540ba6f549b1e6aa3d29edda381100a8
Joshua Campos Gutiérrez <66110357+Joshdroid250@users.noreply.github.com>
2025-05-15 20:52:52 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,9 @@
 const mysql = require('mysql2');
-
-// Puedes cargar dotenv si estás en local
-require('dotenv').config();
-
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT // ¡Importante!
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'lLwudoWrWkumLuZhEgFNsJsLhDCqQhXe',
+  database: 'railway'
 });
 
 connection.connect(err => {
